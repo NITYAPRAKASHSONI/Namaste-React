786948e71daa03751064c4a63a80c22032cd639e
nityaprakashsoni <npsoni86051@gmail.com>
2023-09-11 10:35:08 +0530

    First Lecture Of  Namaste React

---
+++ 01_Lecture/app.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+    "h1",
+    {},
+    "Namaste React from React CDN"
+);
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
